08dfd8ff7c5846113cc81a2cfc7a25c1a415f2f1
Paweł Bylica <chfast@gmail.com>
2020-11-09 11:43:08 +0100

    WIP Use PAPI to count instructions' CPU cycles

--- lib/evmone/execution.cpp
+++ lib/evmone/execution.cpp
@@ -4,15 +4,14 @@
 
 #include "execution.hpp"
 #include "analysis.hpp"
-#include <chrono>
+#include <papi.h>
+#include <iostream>
 #include <memory>
 
 namespace evmone
 {
-using clock = std::chrono::steady_clock;
-
-clock::time_point end_time;
-
+long_long start_cycles;
+long_long end_cycles;
 
 evmc_result execute(evmc_vm* /*unused*/, const evmc_host_interface* host, evmc_host_context* ctx,
     evmc_revision rev, const evmc_message* msg, const uint8_t* code, size_t code_size) noexcept
@@ -22,16 +21,19 @@ evmc_result execute(evmc_vm* /*unused*/, const evmc_host_interface* host, evmc_h
     auto state = std::make_unique<execution_state>(*msg, rev, *host, ctx, code, code_size);
     state->analysis = &analysis;
 
+    start_cycles = PAPI_get_real_cyc();
+
     const auto* instr = &state->analysis->instrs[0];
     while (instr != nullptr)
     {
-        end_time = clock::now();
+        end_cycles = PAPI_get_real_cyc();
         instr = instr->fn(instr, *state);
     }
 
     const auto gas_left =
         (state->status == EVMC_SUCCESS || state->status == EVMC_REVERT) ? state->gas_left : 0;
 
+    std::cerr << "Total cycles: " << (end_cycles - start_cycles) << "\n";
     return evmc::make_result(
         state->status, gas_left, &state->memory[state->output_offset], state->output_size);
 }
